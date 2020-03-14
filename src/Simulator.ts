9d63e2b146750876d7b99175c8038298917e7ce9
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-14 04:20:28 +0100

    removed unused import and added new

--- src/Simulator.ts
+++ src/Simulator.ts
@@ -1,6 +1,6 @@
 import { CellOfParticles } from "./CellOfParticles";
 import { GridPosition } from "./GridPosition";
-import * as Collections from "typescript-collections";
+import { Grid } from "./Grid";
 
 export class Simulator {
   readonly TRESHOLD_FOR_SPLIT = 1000;
