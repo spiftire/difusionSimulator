1d341a9f4cb3092fb71b6a13e9e7705ea73646b7
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-14 04:20:08 +0100

    Moved chanses init

--- src/Simulator.ts
+++ src/Simulator.ts
@@ -11,24 +11,21 @@ export class Simulator {
   chances: Map<Direction, number>;
 
   constructor(
-    // cells: Array<CellOfParticles>,
-    grid: Array<Array<CellOfParticles>>
-  ) {
+    this.chances = new Map();
+    this.chances.set(Direction.Left, 20);
+    this.chances.set(Direction.Right, 20);
+    this.chances.set(Direction.Down, 30);
+    this.chances.set(Direction.Up, 5);
+    this.chances.set(Direction.Stay, 25);
+
     this.grid = grid;
-    // this.cells = cells;
-    this.totalChanse = this.sumUpChances(this.chances);
+    this.totalChanse = this.sumUpChances();
     let firstCell = new CellOfParticles(
       10000,
       new GridPosition(Math.floor(grid[0].length / 2), 0)
     );
 
     grid[firstCell.position.y][firstCell.position.x] = firstCell;
-
-    this.chances.set(Direction.Left, 20);
-    this.chances.set(Direction.Right, 20);
-    this.chances.set(Direction.Down, 30);
-    this.chances.set(Direction.Up, 5);
-    this.chances.set(Direction.Stay, 25);
   }
 
   init() {
