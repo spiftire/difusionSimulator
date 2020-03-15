637d2400db02e10f4ffc4e5202fde220d5aea842
Steffen Holanger <holanger.steffen@gmail.com>
2020-03-15 05:22:56 +0100

    commenting, refactoring and removing logging

--- src/Simulator.ts
+++ src/Simulator.ts
@@ -27,7 +27,7 @@ export class Simulator {
     this.BOTTOM_EDGE = this.grid.numberOfRows;
 
     this.totalChanse = this.sumUpChances();
-    let firstCell = new CellOfParticles(10000, new GridPosition(8, 0));
+    let firstCell = new CellOfParticles(10000, new GridPosition(0, 0));
 
     grid.setContentAtPosition(firstCell.position.x, firstCell.position.y, firstCell);
   }
@@ -38,22 +38,18 @@ export class Simulator {
 
   step() {
     let oldGrid = this.grid;
-    // console.table(oldGrid.grid);
+
     // create new array from old
     const newGrid = new Grid(oldGrid.numberOfRows, oldGrid.numberOfColums);
 
     let numbOfColums = oldGrid.numberOfColums;
     let numbOfRows = oldGrid.numberOfRows;
-    /// loop thoough old array
-    let counter = 0;
+
+    // loop thoough old array
     for (let y = 0; y < numbOfColums; y++) {
       for (let x = 0; x < numbOfRows; x++) {
-        counter++;
-        // console.warn("loop counter" + counter);
         let cell = oldGrid.getPositionContent(x, y);
         if (cell != null && cell.numberOfParticles > 0) {
-          // console.log(cell);
-
           const numberOfParticles = cell.numberOfParticles;
 
           // looping through all the chanses to get split amount
@@ -64,11 +60,10 @@ export class Simulator {
             // put new cells into position
             let newPosition = this.getNewCellPosition(<Direction>(<unknown>direction), cell.position);
 
-            // if there is something at that position add it
+            // create new cell from split
             let newCell = this.splitCell(splitAmount, cell, newPosition);
-            if (cell.numberOfParticles <= 0) {
-              cell = null;
-            }
+
+            // if there is something at that position add it
             this.mergeParticleInCell(newCell, oldGrid, newGrid);
           });
         }
@@ -78,19 +73,23 @@ export class Simulator {
     this.grid = newGrid;
   }
 
+  /**
+   * Merge the new cell into the existing one
+   * check if there already is a cell at new position
+   * if yes merge them, if no insert new cell.
+   * if it is not posible to move stay on current position, check if there is particles in ocupied spot (in new grid)
+   * if yes merge them, if not insert.
+   * @param newCell the cell to insert
+   * @param previusGrid The old grid from last itteration (t-1)
+   * @param newGrid The new grid from this itteration (t)
+   */
   mergeParticleInCell(newCell: CellOfParticles, previusGrid: Grid, newGrid: Grid) {
-    // check if there already is a cell at new position
-    // if yes merge them, if no insert new cell.
-    // if it is not posible to move stay on current position, check if there is particles in ocupied spot (in new grid)
-    // if yes merge them, if not insert.
-
     const x: number = newCell.position.x;
     const y: number = newCell.position.y;
     let result: CellOfParticles = newCell;
-    const cellAlreadyThere = previusGrid.getPositionContent(x, y);
-    const cellInNewGrid = newGrid.getPositionContent(x, y);
+    const cellAlreadyThere = newGrid.getPositionContent(x, y);
 
-    if (cellAlreadyThere == null) {
+    if (cellAlreadyThere === null) {
       newGrid.setContentAtPosition(x, y, newCell);
     }
 
@@ -101,12 +100,14 @@ export class Simulator {
     }
   }
 
+  /**
+   * Tries to move into new position. If we are on the edge we stay there
+   * @param direction The direction that we want to move
+   * @param startPosition The position that we are moving from
+   */
   getNewCellPosition(direction: Direction, startPosition: GridPosition): GridPosition {
     const x = startPosition.x;
     const y = startPosition.y;
-    // console.log("y: " + y);
-    // console.log("x: " + x);
-    // console.log(direction);
 
     let newX: number = x,
       newY: number = y;
@@ -143,25 +144,44 @@ export class Simulator {
 
         break;
     }
-    // console.log(`ǸewX: ${newX}, newY: ${newY}`);
 
     return new GridPosition(newX, newY);
   }
 
+  /**
+   * Calculates the amount of particles to split of as a precentage of the chance of movment
+   * @param orgAmount The original amount of particles
+   * @param direction The direction we want to move
+   */
   calculateAmountToSplit(orgAmount: number, direction: Direction): number {
     let chance = 0;
     if (orgAmount > this.TRESHOLD_FOR_SPLIT) {
       chance = this.chances.get(direction);
     }
 
-    return (orgAmount * chance) / this.totalChanse;
+    return Math.ceil((orgAmount * chance) / this.totalChanse);
   }
 
+  /**
+   * Creates a new cell and filles with a number of particles that is split off from old cell
+   * @param numberToSplitAway the amount of particles to split off
+   * @param cellToSplit the cell that should be split
+   * @param positionOfNewCell the position of the new cell
+   */
   splitCell(numberToSplitAway: number, cellToSplit: CellOfParticles, positionOfNewCell: GridPosition): CellOfParticles {
+    if (numberToSplitAway <= this.TRESHOLD_FOR_SPLIT) {
+      return cellToSplit;
+    }
+    if(numberToSplitAway> cellToSplit.numberOfParticles) {
+      numberToSplitAway = cellToSplit.numberOfParticles;
+    }
     cellToSplit.numberOfParticles -= numberToSplitAway;
     return new CellOfParticles(numberToSplitAway, positionOfNewCell);
   }
 
+  /**
+   * Sums up the total chance
+   */
   sumUpChances(): number {
     let totalChance = 0;
     this.chances.forEach((chance, _) => {
